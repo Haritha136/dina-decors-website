840fc80909774dfc3bd6d02e8c7b944d2ff2b0ac
minnu-cyber <harithabs136@gmail.com>
2026-07-17 14:10:37 +0530

    Footer changes

--- src/componentss/footer.jsx
+++ src/componentss/footer.jsx
@@ -1,13 +1,16 @@
+import logo from "../assetso/logo2.jpeg";
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-logo">
+      <img src={logo2} alt="Dina Decors Logo" />
+      </div>
       <h3>Dina Decors and Event Planners</h3>
 
       <p>
         Creating memorable events with elegance and style.
       </p>
-
-      <p>
+      <p className="copyright">
         © 2026 Dina Decors and Event Planners. All Rights Reserved.
       </p>
     </footer>
