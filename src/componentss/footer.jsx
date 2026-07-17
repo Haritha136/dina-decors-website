a183feff87177aec0dd7ef7d8db8c06fe1474cfb
minnu-cyber <harithabs136@gmail.com>
2026-07-17 14:38:26 +0530

    Footer number

--- src/componentss/footer.jsx
+++ src/componentss/footer.jsx
@@ -1,15 +1,20 @@
-import logo2 from "../assetso/logo2.jpeg";
+import logo7 from "../assetso/logo7.jpeg";
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-logo">
-      <img src={logo2} alt="Dina Decors Logo" />
+      <img src={logo7} alt="Dina Decors Logo" />
       </div>
-      <h3>Dina Decors and Event Planners</h3>
+      <h3>Dina Decors & Event Planners</h3>
 
       <p>
         Creating memorable events with elegance and style.
       </p>
+      <div className="footer-contact">
+        <p>📞 +91 8113917460, +91 8111900460</p>
+        <p>✉️ dinadecors84@gmail.com</p>
+        <p>📍 Kollam, Kerala</p>
+      </div>
       <p className="copyright">
         © 2026 Dina Decors and Event Planners. All Rights Reserved.
       </p>
